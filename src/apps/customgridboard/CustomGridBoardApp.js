77ac439c1ad1cd56bf34b9465103aaea03d48136
Wade Peterson <wpeterson@rallydev.com>
2015-01-06 08:52:20 -0700

    Added CustomGridBoardApp

---
+++ src/apps/customgridboard/CustomGridBoardApp.js
@@ -0,0 +1,29 @@
+(function () {
+    var Ext = window.Ext4 || window.Ext;
+
+    Ext.define('Rally.apps.customgridboard.CustomGridBoardApp', {
+        extend: 'Rally.app.GridBoardApp',
+        alias: 'widget.customgridboardapp',
+        requires: ['Rally.ui.gridboard.plugin.GridBoardCustomView'],
+        statePrefix: 'customGridBoard',
+        config: {
+            enableGridBoardToggle: true,
+            defaultSettings: {
+                modelNames: ['hierarchicalrequirement']
+            }
+        },
+
+        loadModelNames: function () {
+            return Deft.Promise.when(this.getSetting('modelNames'));
+        },
+
+        getGridBoardPlugins: function () {
+            return this.callParent(arguments).concat([
+                {
+                    ptype: 'rallygridboardcustomview',
+                    stateId: this.getStateId()
+                }
+            ]);
+        }
+    });
+})();
