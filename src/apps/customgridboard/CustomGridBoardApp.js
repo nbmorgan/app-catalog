7081fbf64544af0ae2e364a4dfccba349d2a5462
Wade Peterson <wpeterson@rallydev.com>
2015-01-07 16:48:58 -0700

    Update CustomGridBoardApp to use multi Model picker

--- src/apps/customgridboard/CustomGridBoardApp.js
+++ src/apps/customgridboard/CustomGridBoardApp.js
@@ -5,8 +5,8 @@
         extend: 'Rally.app.GridBoardApp',
         alias: 'widget.customgridboardapp',
         requires: [
-            'Rally.ui.gridboard.plugin.GridBoardCustomView',
-            'Rally.ui.gridboard.plugin.GridBoardModelPicker'
+            'Rally.ui.Button',
+            'Rally.ui.gridboard.plugin.GridBoardCustomView'
         ],
         statePrefix: 'customGridBoard',
         config: {
@@ -17,7 +17,52 @@
         },
 
         loadModelNames: function () {
-            return Deft.Promise.when(this.getSetting('modelNames'));
+            return Deft.Promise.when(this.getSetting('modelNames') || []);
+        },
+
+        loadGridBoard: function () {
+            if (this.modelNames.length) {
+                this.callParent(arguments);
+            } else {
+                Ext.create('Rally.ui.popover.Popover', {
+                    target: this.getEl(),
+                    items: [
+                        this._createModelPicker(),
+                        {
+                            xtype: 'rallybutton',
+                            itemId: 'acceptbutton',
+                            text: 'Accept',
+                            style: {
+                                float: 'right'
+                            }
+                        }
+                    ]
+                });
+            }
+        },
+
+        getHeaderControls: function () {
+            return this.callParent(arguments).concat(this._createModelPicker());
+        },
+
+        _createModelPicker: function (options) {
+            this.modelPicker = Ext.create('Rally.ui.picker.ModelTypePicker', _.merge({
+                margin: '0 9 0 0',
+                value: this.modelNames,
+                width: 200,
+                listeners: {
+                    collapse: function (picker) {
+                        var selectedModels = picker.getSubmitValue();
+                        if (selectedModels.length && _.difference(selectedModels, this.modelNames).length > 0) {
+                            picker.resetOriginalValue();
+                            this.modelNames = selectedModels;
+                            this.loadGridBoard();
+                        }
+                    },
+                    scope: this
+                }
+            }, options));
+            return this.modelPicker;
         },
 
         getGridBoardPlugins: function () {
@@ -25,10 +70,6 @@
                 {
                     ptype: 'rallygridboardcustomview',
                     stateId: this.getStateId()
-                },
-                {
-                    ptype: 'rallygridboardmodelpicker',
-                    stateId: this.getScopedStateId('model-picker')
                 }
             ]);
         }
