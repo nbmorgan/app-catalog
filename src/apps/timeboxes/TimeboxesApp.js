2c5044428eaa44dcb0e0c3a20d992f54e340491b
Garston Tremblay, Jonathan Baker and Wade Peterson <gtremblay@rallydev.com>
2015-02-17 11:15:14 -0700

    Attempt at A2 charts on Timeboxes app

--- src/apps/timeboxes/TimeboxesApp.js
+++ src/apps/timeboxes/TimeboxesApp.js
@@ -9,7 +9,12 @@
 
     Ext.define('Rally.apps.timeboxes.TimeboxesApp', {
         extend: 'Rally.app.GridBoardApp',
-        require: ['Deft.Deferred'],
+        require: [
+            'Deft.Deferred',
+            'Rally.apps.chartbuilder.EaselAlmBridge',
+            'Rally.ui.combobox.MilestoneComboBox',
+            "Rally.apps.chartbuilder.StateFieldPicker",
+            'Rally.util.Help'],
 
         enableGridBoardToggle: true,
 
@@ -24,6 +29,21 @@
                     scope: this
                 });
             }
+        },
+
+        getSettingsFields: function () {
+            debugger;
+            var fields = this.callParent(arguments) || [];
+            // get the settings fields that the chart requires
+            var listOfTransformedSettings = this.almBridge.getSettingsFields();
+            return fields.concat(listOfTransformedSettings);
+        },
+
+        getDefaultSettings: function() {
+            var defaults = this.callParent(arguments);
+            var defaultsFromEaselChart = this.almBridge.getDefaultSettings();
+            debugger;
+            return Ext.apply(defaults, defaultsFromEaselChart, {} );
         },
 
         changeModelType: function (newType) {
@@ -42,8 +62,43 @@
             });
         },
 
+        showSettings: function() {
+            // this feels like a hack
+            this.owner.defaultSettings = this.getDefaultSettings();
+            //this.owner.showSettings();
+        },
+
         getChartConfig: function () {
             if (_.contains(this.modelNames, 'iteration')) {
+                return {
+                    xtype:'rallyd3chart',
+                    config: this._getChartConfig()
+                }
+            }
+            else if (_.contains(this.modelNames, 'milestone')) {
+                return {
+                    itemId: 'mrcontainer',
+                    xtype: 'component',
+                    listeners: {
+                        afterrender: function(){
+                            this.almBridge = Ext.create('Rally.apps.chartbuilder.EaselAlmBridge', {
+                                chartType : 'milestone-burn',
+                                app: this
+                            });
+                            this.down("#mrcontainer").el.dom.firstChild.almBridge = this.almBridge;
+                            this.almBridge.registerSettingsFields([
+                                { type: 'milestone-picker', name: 'milestone', label: 'Milestone', value: '22897900071'}
+                            ])
+                        },
+                        scope: this
+                    },
+                    style: {
+                        overflow: 'hidden'
+                    },
+                    html: this.constructIFrame()
+                };
+            }
+            else if (_.contains(this.modelNames, 'release')) {
                 return {
                     xtype: 'component',
                     style: {
@@ -52,6 +107,28 @@
                     html: '<iframe style="width: 100%; height: 100%" src="'+ Rally.environment.getServer().getContextPath() +'/charts/iterationVelocityChart.sp"></iframe>'
                 };
             }
+        },
+
+        constructIFrame: function() {
+            var filename = this.isDebugMode() ? 'almchart.html' : 'almchart.min.html';
+            var version = this.getChartVersionFromRequest();
+            var url = '/analytics/chart/' + version + '/' + filename + '?_gen=' + this._getCacheGeneration();
+            return '<iframe frameborder="0" style="overflow:hidden;" scrolling="no" width="100%" height="100%" src="' + url + '"></iframe>';
+        },
+
+        getChartVersionFromRequest: function() {
+            var parameters = Ext.Object.fromQueryString(location.search || '');
+            return (parameters.chartVersion || 'releases/current');
+        },
+
+        _getCacheGeneration : function(theDate) {
+            theDate = theDate || new Date();
+            return Ext.Date.format(theDate, 'YmdH');
+        },
+
+        isDebugMode: function() {
+            var parameters = Ext.Object.fromQueryString(location.search || '');
+            return parameters.packtag === 'false';
         },
 
         _createPicker: function () {
