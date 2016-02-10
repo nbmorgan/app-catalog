68fd2373a20c3fecd1fa9303c1383e15886a9e67
Tyler Knodell and Matt Bajor <tknodell@rallydev.com>
2016-02-10 14:56:52 -0700

    recreate the dir

--- npm_link_appcatalog_deps.sh
+++ npm_link_appcatalog_deps.sh
@@ -2,7 +2,9 @@
 
 rm -rf bin/sencha lib/ext/4.2.2 lib/webdriver
 
+mkdir -p lib/ext
 ln -fns ../../node_modules/rally-ext-lean lib/ext/4.2.2
 rm -f lib/ext/4.2.2/package.json
+
 cp -rp node_modules/rally-sencha-cmd bin/sencha
 ln -fns ../node_modules/rally-webdriver lib/webdriver
