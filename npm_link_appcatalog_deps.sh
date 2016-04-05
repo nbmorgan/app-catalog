aba5db2be8bf3acff93c7a6617eea0f020993d69
Matt Bajor <mbajor@rallydev.com>
2016-04-05 15:05:55 -0600

    Upgrade our link script to have some smarts

--- npm_link_appcatalog_deps.sh
+++ npm_link_appcatalog_deps.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
-rm -rf bin/sencha lib
-
 function cp_with_exclude() {
   echo "Copying ${1##*/} into $2..."
-  rsync -a --exclude=node_modules $1/ $2/
+  mkdir -p $2
+  rsync -a --delete --exclude=node_modules $1/ $2/
 }
 
-mkdir -p lib/ext bin/
 cp_with_exclude node_modules/rally-ext-lean lib/ext/4.2.2
-rm -f lib/ext/4.2.2/package.json
-
 cp_with_exclude node_modules/rally-sencha-cmd bin/sencha
 cp_with_exclude node_modules/rally-webdriver lib/webdriver
 cp_with_exclude node_modules/rally-appsdk/rui lib/sdk
+
+rm -f lib/ext/4.2.2/package.json
+
