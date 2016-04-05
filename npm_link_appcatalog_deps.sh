562e332e79ee294a048b09d2de450f3a4bb13cc9
mrampson <mrampson@gmail.com>
2016-04-05 13:42:35 -0600

    speed up npm linking and exclude node_module directories
    * speed up npm linking and exclude node_module directories

--- npm_link_appcatalog_deps.sh
+++ npm_link_appcatalog_deps.sh
@@ -2,10 +2,15 @@
 
 rm -rf bin/sencha lib
 
+function cp_with_exclude() {
+  echo "Copying ${1##*/} into $2..."
+  rsync -a --exclude=node_modules $1/ $2/
+}
+
 mkdir -p lib/ext bin/
-cp -rp node_modules/rally-ext-lean lib/ext/4.2.2
+cp_with_exclude node_modules/rally-ext-lean lib/ext/4.2.2
 rm -f lib/ext/4.2.2/package.json
 
-cp -rp node_modules/rally-sencha-cmd bin/sencha
-cp -rp node_modules/rally-webdriver lib/webdriver
-cp -rp node_modules/rally-appsdk/rui lib/sdk
+cp_with_exclude node_modules/rally-sencha-cmd bin/sencha
+cp_with_exclude node_modules/rally-webdriver lib/webdriver
+cp_with_exclude node_modules/rally-appsdk/rui lib/sdk
